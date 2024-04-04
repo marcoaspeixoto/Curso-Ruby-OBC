99475683c593448d74316689a53c535fb4ab19a2
Marco Peixoto <marcoaspeixoto@gmail.com>
2024-04-04 18:35:45 -0300

    4 argv

---
+++ ArquivoGems/4-argv.rb
@@ -0,0 +1,16 @@
+# # 1-Utilizando Argumentos
+# ARGV.each { |arg| puts arg }
+# ARGV.each { |arg| puts arg.class }
+
+# # 2-Potência de um número
+# ARGV.each do |arg|
+#   pot = arg.to_i ** 2
+#   puts "#{arg} ao quadrado é: #{pot}"
+# end
+
+# 3-Salvando conteúdo de argumento em arquivo
+File.open("data/games.txt", "a") do |file|
+  ARGV.each do |game|
+    file.puts game
+  end
+end
