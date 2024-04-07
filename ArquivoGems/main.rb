4accf2bce6dc2b80f0ed3ca91221cc6780f9656a
Marco Peixoto <marcoaspeixoto@gmail.com>
2024-04-07 11:25:57 -0300

    main

---
+++ ArquivoGems/main.rb
@@ -0,0 +1,6 @@
+# require './modulo'
+require_relative 'modulo'
+
+puts LengthConversions.miles_to_feet(10)
+puts LengthConversions.miles_to_inches(20)
+puts LengthConversions.miles_to_centimers(30)
