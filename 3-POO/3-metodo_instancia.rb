e544ace708373bbdb822790c241df23836b94e0e
Marco Peixoto <marcoaspeixoto@gmail.com>
2024-04-11 22:14:58 -0300

    3 - método instância

---
+++ 3-POO/3-metodo_instancia.rb
@@ -0,0 +1,25 @@
+class Game
+  def initialize
+    puts "Novo objeto de jogo foi criado"
+    # Variável de Instância
+    @name = "Pes"
+    @genre = "Esporte"
+    @multiplayer = true
+    @versions = [2018, 2019, 2020]
+    @price = 100
+  end
+  def information
+    puts "O jogo é do gênero #{@genre} e as versões: #{@versions}"
+  end
+
+  def techinical_sheet
+    puts "##Dados do Jogo##"
+    puts "Nome do Jogo: #{@name}"
+    puts "Gênero do Jogo: #{@genre}"
+    puts "Preço do Jogo: #{@price}"
+  end
+end
+
+game = Game.new
+puts game.information
+puts game.techinical_sheet
