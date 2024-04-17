4dff469cc71b4da09212a545957795cfb6966c50
Marco Peixoto <marcoaspeixoto@gmail.com>
2024-04-17 16:47:31 -0300

    14 - herança

---
+++ 3-POO/14-heranca.rb
@@ -0,0 +1,41 @@
+class Game
+  attr_accessor :name, :genre, :price # Leitura e escrita
+  attr_reader :multiplayer, :versions
+
+  def initialize(name, genre, multiplayer, versions, price)
+    # Variável de Instância
+    @name = name
+    @genre = genre
+    @multiplayer = multiplayer
+    @versions = versions
+    @price = price
+  end
+
+  def to_s
+    "#{@name}-#{@genre}-#{@multiplayer}-#{@versions}"
+  end
+
+  def techinical_sheet
+    puts "##Dados do Jogo##"
+    puts "Nome do Jogo: #{@name}"
+    puts "Gênero do Jogo: #{@genre}"
+    puts "Preço do Jogo: #{@price}"
+  end
+end
+
+class DownloadableGame < Game
+  attr_accessor :download_size
+
+  def initialize(name, genre, multiplayer, versions, price, download_size)
+    super(name, genre, multiplayer, versions, price)
+    @download_size = download_size
+  end
+end
+
+game1 = Game.new("Fifa", "Esporte", true, [2018, 2019, 2020], 100)
+puts game1
+puts game1.techinical_sheet
+
+game2 = DownloadableGame.new("Call of Duty", "Ação", true, ["Black Ops"], 100, 60)
+puts game2
+puts game2.techinical_sheet
