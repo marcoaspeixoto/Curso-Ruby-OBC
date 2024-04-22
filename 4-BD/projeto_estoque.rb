867ea5731402ba7536a570efc908157d278ed5bb
Marco Peixoto <marcoaspeixoto@gmail.com>
2024-04-22 20:23:21 -0300

    recuperação de dados

--- 4-BD/projeto_estoque.rb
+++ 4-BD/projeto_estoque.rb
@@ -1,6 +1,6 @@
-equire 'active_record'
+require 'active_record'
 
-# # 1-Configuração do BD
+# 1-Configuração do BD
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
   host: 'localhost',
@@ -32,7 +32,7 @@ class Product < ActiveRecord::Base
 
   def decrease_stock(amount)
     if self.stock_quantity >= amount
-      self.strock_quantity -= amount
+      self.stock_quantity -= amount
       self.save
     else
       puts "Não há estoque suficiente para realizar a operação"
@@ -43,5 +43,20 @@ class Product < ActiveRecord::Base
     self.stock_quantity += amount
     self.save
   end
+end
+
+# 4-Utilizando os Modelos
+category = Category.create(name: 'Eletrônicos')
+
+product1 = category.products.create(name: 'Smartphone', stock_quantity: 10)
+product2 = category.products.create(name: 'Notebook', stock_quantity: 5)
+
+product1.decrease_stock(3)
+product2.increase_stock(5)
+
+# 5-Recuperação dos Dados
+all_products = Product.all
+all_products.each do |product|
+  puts "Nome: #{product.name}, Categoria: #{product.category.name}, Estoque: #{product.stock_quantity}"
 end
 
