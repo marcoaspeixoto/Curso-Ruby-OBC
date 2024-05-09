852a3f01112aa12892db18c03261827751e84472
Marco Peixoto <marcoaspeixoto@gmail.com>
2024-05-08 22:51:01 -0300

    9 - miscelanea segundo exemplo

--- 5-Avancado/9-miscelanea.rb
+++ 5-Avancado/9-miscelanea.rb
@@ -2,9 +2,22 @@
 strings = ["apple", "cd", "cherry", "dt", "grape", "fig"]
 
 strings.each do |string|
-  if string =~ /[^aeiou]/
+  if string =~ /^[^aeiou]+$/
     puts "#{string} contém apenas consoantes."
   else
     puts "#{string} contém vogais ou outros caracteres."
   end
 end
+
+# 2- # Exemplo de uso de sub e gsub em Ruby para substituir partes de uma string.
+string = "A linguagem Ruby é fácil de aprender e Ruby é divertida."
+novo_string_sub = string.sub(/Ruby/, 'Python')
+novo_string_gsub = string.gsub(/Ruby/, 'Python')
+
+puts "Usando sub:"
+puts "String original: #{string}"
+puts "String modificada com sub: #{novo_string_sub}"
+
+puts "\nUsando gsub:"
+puts "String original: #{string}"
+puts "String modificada com gsub: #{novo_string_gsub}"
