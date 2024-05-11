f2f91c6a5f58971d1f98113d665cde56359c0f9c
Marco Peixoto <marcoaspeixoto@gmail.com>
2024-05-10 22:38:21 -0300

    exercicio 2 continuação

--- 5-Avancado/exercicio_2_verificacao_conversao_data.rb
+++ 5-Avancado/exercicio_2_verificacao_conversao_data.rb
@@ -1,6 +1,17 @@
-puts "Digite uma data no formato dd/mm/aaaa:"
+# Verificação e conversão de formato de data em uma string usando expressões regulares em Ruby.
+def verificar_e_converter_data(string)
+  if string =~ /\A(\d{2})\/(\d{2})\/(\d{4})\z/
+    dia, mes, ano = string.scan(/\d+/)
+    nova_data = "#{ano}-#{mes}-#{dia}"
+    puts "A data no formato 'dd/mm/aaaa' é válida e convertida para o formato 'aaaa-mm-dd': #{nova_data}"
+  else
+    puts "A data inserida não está no formato correto 'dd/mm/aaaa'."
+  end
+end
+
+# Solicita ao usuário uma data no formato "dd/mm/aaaa"
+puts "Insira uma data no formato 'dd/mm/aaaa': "
 data = gets.chomp
 
-if data =~ /^[\d\d\d\d\/\d\d\/\d\d]$/
-  puts "data"
-end
+# Chama a função para verificar e converter a data
+verificar_e_converter_data(data)
